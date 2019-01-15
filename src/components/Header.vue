d556ad555db1fba804443a725997913d0664d2b9
imjiang <410541515@qq.com>
2019-01-15 21:38:12 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="comm_layout_header" class="layout  none" data-wow-duration='0s' data-wow-delay='0s' data-wow-offset='0' data-wow-iteration='1'>
+    <div class="view_contents">
+      <div id="dh_style_01_1494040258544" class="view style_01 dh  none lockHeightView" data-wow-duration='0s' data-wow-delay='0s' data-wow-offset='0' data-wow-iteration='1'>
+        <div names="dh" class="view_contents">
+          <div id="menu" class="menu menuStyle_1">
+            <div class="menuLayout">
+              <ul class="miniMenu columnSet showmobile">
+                <li class="leftBox">
+                  <div class="nav"></div>
+                  <div class="sidebar icoMenuSet" onclick="setDhListen('style_01',this)">=</div>
+                  <div class="menuScroll">
+                    <ul class="menuUlCopy">
+                      <li id="hot" class="rflex" pageid="61843" showmobile="">
+                        <a class="mainMenuSet" href="index.html">首页</a>
+                      </li>
+                      <li class="rflex" pageid="61844" showmobile="">
+                        <a class="mainMenuSet" href="61844-61844.html">联系</a>
+                      </li>
+                      <li class="rflex" pageid="61845" showmobile="">
+                        <a class="mainMenuSet" href="61845-61845.html">新闻</a>
+                      </li>
+                      <li class="rflex" pageid="61846" showmobile="">
+                        <a class="mainMenuSet" href="61846-61846.html">关于</a>
+                      </li>
+                      <li class="rflex" pageid="61847" showmobile="">
+                        <a class="mainMenuSet" href="61847-61847.html">案例</a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+              <div class="menuUl_box columnSet showpc">
+                <ul class="menuUl dflex">
+                  <li id="hot" class="rflex" pageid="61843" showpc="">
+                    <a class="mainMenuSet" href="index.html">首页</a>
+                  </li>
+                  <li class="rflex" pageid="61844" showpc="">
+                    <a class="mainMenuSet" href="61844-61844.html">联系</a>
+                  </li>
+                  <li class="rflex" pageid="61845" showpc="">
+                    <a class="mainMenuSet" href="61845-61845.html">新闻</a>
+                  </li>
+                  <li class="rflex" pageid="61846" showpc="">
+                    <a class="mainMenuSet" href="61846-61846.html">关于</a>
+                  </li>
+                  <li class="rflex" pageid="61847" showpc="">
+                    <a class="mainMenuSet" href="61847-61847.html">案例</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- 菜单区域 End-->
+        </div>
+      </div>
+      <div id="image_logo_1494040027010" class="view logo image  none" data-wow-duration='0s' data-wow-delay='0s' data-wow-offset='0' data-wow-iteration='1'>
+        <div names="image" class="view_contents">
+          <div class="logoStyle modSet">
+            <a href='index.html' target="_self"><img class="imgSet" src="img\logo.png?1505469164" title="" alt="" style="width:100%; height:auto"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
